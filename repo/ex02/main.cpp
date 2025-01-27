8395b660a104549e33dafc257268c1251f56abf0
Quentin Hauuy <quentinhauuy@hotmail.com>
2025-01-27 18:27:56 +0100

    ex02

---
+++ repo/ex02/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string string = "HI THIS IS BRAIN";
+	std::string* stringPTR = &string;
+	std::string& stringREF = string;
+
+	std::cout << "memory address of string : " << &string << std::endl;
+	std::cout << "memory address held by stringPTR : " << stringPTR << std::endl;
+	std::cout << "memory address held by stringREF : " << &stringREF << std::endl;
+
+	std::cout << "string : " << string << std::endl;
+	std::cout << "value pointed to by stringPTR : " << *stringPTR << std::endl;
+	std::cout << "value pointed to by stringREF : " << stringREF << std::endl;
+}
