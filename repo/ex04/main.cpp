ccb95badedff738cb1fe220bb7cab9a64e83e1e9
Quentin Hauuy <quentinhauuy@hotmail.com>
2025-01-29 14:17:33 +0100

    normalement c'est bon pour l'exo 04

--- repo/ex04/main.cpp
+++ repo/ex04/main.cpp
@@ -23,42 +23,40 @@ static std::string getFileContent(const std::string& filename)
 
 static std::string replace(const std::string& s, const std::string& s1, const std::string& s2)
 {
-	std::ostringstream	oss;
-	size_t				pos = 0;
+	std::string	result;
+	size_t		i;
+	size_t		j;
 
-	while ((pos = s.find(s1, pos)) != std::string::npos)
+	i = 0;
+	while ((j = s.find(s1, i)) != std::string::npos)
 	{
-		std::cout << "found at " << pos << std::endl;
-		oss << s.substr(0, pos) << s2;
-		
-		pos++;
+		result.append(s, i, j - i);
+		result.append(s2);
+		i = j + s1.size();
 	}
-	(void)s2;
-	return oss.str();
+	result.append(s, i, s.size() - i);
+	return result;
+}
+
+static void createOutputFile(const std::string& inputFilename, const std::string& content)
+{
+	std::ofstream f(inputFilename + ".replace");
+	f << content;
 }
 
 int main(int argc, char **argv)
 {
-	std::string	inputFilename;
-	std::string	outputFilename;
-	std::string	inputFileContent;
-	std::string	outputFileContent;
-	std::string	s1;
-	std::string	s2;
-
 	if (argc != 4)
 		error("usage: ./ex04 filename s1 s2");
 
-	inputFilename = argv[1];
-	s1 = argv[2];
-	s2 = argv[3];
-	outputFilename = inputFilename + ".replace";
-	inputFileContent = getFileContent(inputFilename);
-	outputFileContent = replace(inputFileContent, s1, s2);
+	std::string inputFilename = argv[1];
+	std::string s1 = argv[2];
+	std::string s2 = argv[3];
 
-	std::cout << "input file content :" << std::endl;
-	std::cout << inputFileContent << std::endl;
-	std::cout << "--------------------" << std::endl;
-	std::cout << "output file content :" << std::endl;
-	std::cout << inputFileContent << std::endl;
+	if (s1.empty())
+		error("s1 cannot be empty");
+
+	std::string inputFileContent = getFileContent(inputFilename);
+	std::string outputFileContent = replace(inputFileContent, s1, s2);
+	createOutputFile(inputFilename, outputFileContent);
 }
