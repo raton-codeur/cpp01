2cd390fe6c8bb65fec766aad374296792ba18491
Quentin Hauuy <quentinhauuy@hotmail.com>
2025-01-29 08:38:28 +0100

    blabla

--- repo/ex00/main.cpp
+++ repo/ex00/main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include "Zombie.hpp"
 
-int main()
+static void local_zombies()
 {
 	Zombie z;
 	Zombie fred("Fred");
 	z.announce();
 	fred.announce();
+}
+
+int main()
+{
+	local_zombies();
 	Zombie* bob = newZombie("Bob");
 	bob->announce();
 	delete bob;
